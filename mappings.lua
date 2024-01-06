7d9c91f97992c2f679c8926ee78dd754a1daecff
janders <jim_anders@intuit.com>
2024-01-06 00:57:56 -0500

    Move all mappings into the correct mappings file

--- mappings.lua
+++ mappings.lua
@@ -1,18 +1,48 @@
 local M = {}
 
+M.disabled = {
+  n = { ["<leader>fw"] = "", ["<leader>cm"] = "", ["<leader>gt"] = "" },
+}
+
 M.dap = {
   plugin = true,
   n = {
-    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>"},
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "Toggle Breakpoint" },
     ["<leader>dus"] = {
-      function ()
-        local widgets = require("dap.ui.widgets");
-        local sidebar = widgets.sidebar(widgets.scopes);
-        sidebar.open();
+      function()
+        local widgets = require "dap.ui.widgets"
+        local sidebar = widgets.sidebar(widgets.scopes)
+        sidebar.open()
       end,
-      "Open debugging sidebar"
-    }
-  }
+      "Open debugging sidebar",
+    },
+  },
+}
+
+M.telescope = {
+  plugin = true,
+  n = {
+    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live Grep" },
+    ["<leader>ts"] = {
+      "<cmd> Telescope treesitter <CR>",
+      "Treesitter Symbols",
+    },
+    ["<leader>fq"] = { "<cmd> Telescope quickfix <CR>", "Quickfix List" },
+    ["<leader>fl"] = { "<cmd> Telescope loclist <CR>", "Location List" },
+    ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "Git Commits" },
+    ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git Branches" },
+    ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "Git Status" },
+  },
+}
+
+M.thor = {
+  n = {
+    ["<leader>ftr"] = {
+      "<cmd> Telescope terraform_doc <CR>",
+      "Terraform Docs",
+    },
+  },
+  i = { ["jj"] = { "<ESC>", "Escape Insert Mode", opts = { nowait = true } } },
 }
 
 return M
